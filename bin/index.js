f566d322a3a091b8738dd6161144a8962f122d80
mawrkus <web@sparring-partner.be>
2021-03-28 17:21:37 +0200

    Various adjustments

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,10 @@
-const quotes = require('./quotes');
+#!/usr/bin/env node
+
+const quotes = require('../quotes.json');
 
 function pickOne(quotes) {
   const rnd = Math.floor(Math.random() * quotes.length);
   return quotes[rnd];
 }
 
-console.log('\n', pickOne(quotes), '\n');
+console.log('\n✨ %s\n', pickOne(quotes));
